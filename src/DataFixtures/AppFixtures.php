c8f910cbb19fe7e78fea6ade9f22d8160fb84cba
TrAsKiN <marchal.simon@gmail.com>
2022-11-21 12:08:02 +0100

    feat: add users fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(
+        private readonly PasswordHasherFactoryInterface $passwordHasher
+    ) {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = (new User())
+            ->setUsername('test')
+            ->setEmail('test@test.test')
+            ->setPassword($this->passwordHasher->getPasswordHasher(User::class)->hash('test'))
+        ;
+        $manager->persist($user);
+
+        $admin = (new User())
+            ->setUsername('admin')
+            ->setEmail('admin@admin.admin')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword($this->passwordHasher->getPasswordHasher(User::class)->hash('admin'))
+        ;
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
