dbc1c554ea2de7ac90b198a9feab2eeb5a452433
TrAsKiN <marchal.simon@gmail.com>
2022-11-29 14:35:24 +0100

    chore: add task creation tests

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DomCrawler\Form;
 
 class TaskControllerTest extends WebTestCase
 {
@@ -19,22 +20,89 @@ class TaskControllerTest extends WebTestCase
     public function testListWhenLoggedInAsUser(): void
     {
         $client = static::createClient();
-        SecurityControllerTest::login($client, ['username' => 'test', 'password' => 'test']);
+        SecurityControllerTest::login($client, 'user');
         $client->request('GET', '/tasks');
 
         $this->assertResponseIsSuccessful();
         $this->assertRouteSame('task_list');
-        $this->assertSelectorNotExists('h2:nth-of-type(2)', 'Tâches anonyme');
+        $this->assertSelectorNotExists('h2:nth-of-type(2)');
     }
 
     public function testListWhenLoggedInAsAdmin(): void
     {
         $client = static::createClient();
-        SecurityControllerTest::login($client, ['username' => 'admin', 'password' => 'admin']);
+        SecurityControllerTest::login($client, 'admin');
         $client->request('GET', '/tasks');
 
         $this->assertResponseIsSuccessful();
         $this->assertRouteSame('task_list');
-        $this->assertSelectorExists('h2:nth-of-type(2)', 'Tâches anonyme');
+        $this->assertSelectorExists('h2:nth-of-type(2)');
+        $this->assertSelectorTextContains('h2:nth-of-type(2)', 'Tâches anonyme');
+    }
+
+    public function testCreateWhenNotLoggedIn(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/tasks/create');
+
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertRouteSame('login');
+    }
+
+    public function testCreateWhenLoggedIn(): void
+    {
+        $client = static::createClient();
+        SecurityControllerTest::login($client, 'user');
+        $crawler = $client->request('GET', '/tasks/create');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertRouteSame('task_create');
+        $this->assertInstanceOf(Form::class, $crawler->selectButton('Ajouter')->form());
+    }
+
+    public function testCreateTaskSuccessfully(): void
+    {
+        $client = static::createClient();
+        SecurityControllerTest::login($client, 'user');
+        $client->request('GET', '/tasks/create');
+
+        $client->submitForm('Ajouter', [
+            'task[title]' => 'Task title',
+            'task[content]' => 'My task content',
+        ]);
+
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertRouteSame('task_list');
+        $this->assertSelectorExists('div.alert.alert-success');
+    }
+
+    public function testCreateTaskWithoutTitle(): void
+    {
+        $client = static::createClient();
+        SecurityControllerTest::login($client, 'user');
+        $client->request('GET', '/tasks/create');
+
+        $client->submitForm('Ajouter', [
+            'task[content]' => 'My task content',
+        ]);
+
+        $this->assertSelectorExists('.has-error #task_title');
+        $this->assertSelectorTextContains('.help-block li', 'Vous devez saisir un titre.');
+    }
+
+    public function testCreateTaskWithoutContent(): void
+    {
+        $client = static::createClient();
+        SecurityControllerTest::login($client, 'user');
+        $client->request('GET', '/tasks/create');
+
+        $client->submitForm('Ajouter', [
+            'task[title]' => 'Task title',
+        ]);
+
+        $this->assertSelectorExists('.has-error #task_content');
+        $this->assertSelectorTextContains('.help-block li', 'Vous devez saisir du contenu.');
     }
 }
