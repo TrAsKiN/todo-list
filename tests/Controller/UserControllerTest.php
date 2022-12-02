4871d8835248229435f2f815f79856d6849082a7
TrAsKiN <marchal.simon@gmail.com>
2022-12-02 15:17:42 +0100

    chore: add user create and edit tests

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -4,6 +4,7 @@ namespace App\Tests\Controller;
 
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DomCrawler\Form;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -39,5 +40,73 @@ class UserControllerTest extends WebTestCase
         $this->assertCount(
             count(static::getContainer()->get(UserRepository::class)->findAll()),
             $crawler->filter('table tbody tr'));
+    }
+
+    public function testUserCreateSuccessfully()
+    {
+        $client = static::createClient();
+        $crawler = $client->request(Request::METHOD_GET, '/users/create');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertRouteSame('user_create');
+        $this->assertInstanceOf(Form::class, $crawler->selectButton('Ajouter')->form());
+
+        $client->submitForm('Ajouter', [
+            'user[username]' => 'newuser',
+            'user[password][first]' => 'password',
+            'user[password][second]' => 'password',
+            'user[email]' => 'newuser@user.user',
+        ]);
+
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertRouteSame('homepage');
+    }
+
+    public function testEditUserWhenNotLoggedIn(): void
+    {
+        $client = static::createClient();
+        $user = static::getContainer()->get(UserRepository::class)->findOneByUsername('newuser');
+        $client->request(Request::METHOD_GET, '/users/'. $user->getId() .'/edit');
+
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertRouteSame('login');
+    }
+
+    public function testEditUserWhenNotSelf(): void
+    {
+        $client = static::createClient();
+        SecurityControllerTest::login($client, 'user');
+        $user = static::getContainer()->get(UserRepository::class)->findOneByUsername('newuser');
+        $client->request(Request::METHOD_GET, '/users/'. $user->getId() .'/edit');
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+    }
+
+    public function testEditUserSuccessfully(): void
+    {
+        $client = static::createClient();
+        $user = SecurityControllerTest::login($client, 'newuser');
+        $client->request(Request::METHOD_GET, '/users/'. $user->getId() .'/edit');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertRouteSame('user_edit');
+        $this->assertRequestAttributeValueSame('id', $user->getId());
+        $this->assertInputValueSame('user[username]', $user->getUsername());
+        $this->assertInputValueSame('user[email]', $user->getEmail());
+
+        $client->submitForm('Modifier', [
+            'user[username]' => 'modifieduser',
+            'user[password][first]' => 'password',
+            'user[password][second]' => 'password',
+            'user[email]' => 'modified@user.user',
+        ]);
+
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertResponseIsSuccessful();
+        $this->assertRouteSame('homepage');
+        $this->assertSelectorExists('div.alert.alert-success');
     }
 }
